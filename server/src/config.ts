7d437728798c08e3e7438538c2bacf73b0096098
unknown <shahriar.khan099@gmail.com>
2024-01-02 13:29:39 +0600

    database deployed

--- server/src/config.ts
+++ server/src/config.ts
@@ -3,7 +3,9 @@ dotenv.config();
 
 const config = {
   PORT: process.env.PORT ?? 4000,
-  DB_URI: process.env.DB_URI ?? 'postgres://postgres:st123@localhost:5432/inventorydb'
+  // DB_URI: process.env.DB_URI ?? 'postgres://postgres:st123@localhost:5432/inventorydb'
+  DB_URI: process.env.DB_URI ?? 'postgres://koyeb-adm:6XiHJZp4Pwnr@ep-green-snowflake-77705458.eu-central-1.aws.neon.tech/koyebdb',
+  // DB_SSL: process.env.DB_SSL === 'true'
 }
 
 export default config;
