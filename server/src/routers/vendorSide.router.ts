1c03b2a08aa758435f4c309533e1dc473fc8c19e
unknown <shahriar.khan099@gmail.com>
2024-01-18 17:32:50 +0600

    modification done

--- server/src/routers/vendorSide.router.ts
+++ server/src/routers/vendorSide.router.ts
@@ -1,25 +1,25 @@
 import express, { Request, Response, Router } from 'express';
 const router = express.Router()
-import axios from 'axios';
+// import axios from 'axios';
 
-router.get("/getAll", async (req: Request, res: Response) => {
-  try {
-    const externalServerUrl = "http://localhost:5000/v1/vendor";
+// router.get("/getAll", async (req: Request, res: Response) => {
+//   try {
+//     const externalServerUrl = "http://localhost:5000/v1/vendor";
 
-    const response = await axios.get(externalServerUrl);
-    const externalData = response.data;
+//     const response = await axios.get(externalServerUrl);
+//     const externalData = response.data;
    
     
-    const selectedData = externalData.vendors.map((vendor: any) => ({
-    }));
-    console.log(selectedData);
+//     const selectedData = externalData.vendors.map((vendor: any) => ({
+//     }));
+//     console.log(selectedData);
 
-    res.send( selectedData);
-  } catch (error: any) {
+//     res.send( selectedData);
+//   } catch (error: any) {
 
-    console.error("Error fetching data:", error.message);
+//     console.error("Error fetching data:", error.message);
    
-  }
-});
+//   }
+// });
 
 export default  router;
