60ded08dbf2fceda6bfe656def96490304490812
Md Shahriar Khan <shahriar.khan099@gmail.com>
2024-02-01 12:03:50 +0600

    testing

--- server/src/middleware/auth.middleware.ts
+++ server/src/middleware/auth.middleware.ts
@@ -20,18 +20,12 @@ export async function authMiddleware(req:AuthRequest, res: Response, next: NextF
 
 export async function restaurantId(req:AuthRequest, res: Response, next: NextFunction) {
     try {
-        let resId = 0;
-        const authHeaders = req.headers["authorization"];
-        if (!authHeaders) return res.status(401).send({ message: "Unauthorized" })
-        const check = await getUserFromToken(authHeaders);
-        if (check) {
-            req.user = check.user;
-            req.token = authHeaders;
-            if(req.user.employeeInformation.restaurantId)
-            resId = req.user.employeeInformation.restaurantId;
+        const resId = req.user?.employeeInformation.restaurantId
+        if(resId) {
             res.status(200).send({ message: resId });
-            console.log(req.user.employeeInformation.restaurantId);
-        } else res.status(403).send({ message: "Forbidden"})
+            console.log(req.user?.employeeInformation.restaurantId);
+        }
+        else res.status(403).send({ message: "Forbidden"})
     } catch (error) {
         console.log(error);
         res.status(401).send({ message: 'Unauthorized'})
